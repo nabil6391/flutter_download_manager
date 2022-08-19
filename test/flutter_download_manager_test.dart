c5a65dbefadee8995426640d01eb7cb7bb00d0a6
Nabil <nabil6391@gmail.com>
2022-08-19 23:50:05 +0800

    Add additional tests cases to support batch download. Try to handle special edge cases.

--- test/flutter_download_manager_test.dart
+++ test/flutter_download_manager_test.dart
@@ -9,6 +9,8 @@ void main() {
   var url4 = "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/sample-mp4-file.mp4";
   var url5 =
       "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4";
+  var url6 = "https://static.gtaf.org/v1/quran/static/translation-db/10";
+
   test('future download', () async {
     var dl = DownloadManager();
 
@@ -24,8 +26,9 @@ void main() {
       print(task.progress.value);
     });
 
-    await dl.c.future;
+    await dl.whenComplete(url4);
   });
+
   test('parallel download', () async {
     var dl = DownloadManager();
 
@@ -51,8 +54,9 @@ void main() {
       print(task3.status.value);
     });
 
-    await dl.c.future;
+    await dl.whenCompletes([url, url2, url3]);
   });
+
   test('cancel download', () async {
     var dl = DownloadManager();
 
@@ -68,8 +72,9 @@ void main() {
       print(task.status.value);
     });
 
-    await dl.c.future;
+    await Future.delayed(Duration(seconds: 10), null);
   });
+
   test('pause and resume download', () async {
     var dl = DownloadManager();
 
@@ -89,6 +94,113 @@ void main() {
       print(task.status.value);
     });
 
-    await Future.delayed(Duration(seconds: 30), null);
+    await Future.delayed(Duration(seconds: 20), null);
+  });
+
+  test('handle empty url', () async {
+    var dl = DownloadManager();
+
+    dl.addDownload("", "");
+
+    DownloadTask? task = dl.getDownload("");
+
+    task?.status.addListener(() {
+      print(task.status.value);
+    });
+
+    await dl.whenComplete("");
+  });
+
+  test('handle empty path', () async {
+    var dl = DownloadManager();
+
+    dl.addDownload(url3, "");
+
+    DownloadTask? task = dl.getDownload(url3);
+
+    task?.status.addListener(() {
+      print(task.status.value);
+    });
+
+    await dl.whenComplete(url3);
+  });
+
+  test('handle url with empty extension', () async {
+    var dl = DownloadManager();
+
+    dl.addDownload(url6, "");
+
+    DownloadTask? task = dl.getDownload(url6);
+
+    task?.status.addListener(() {
+      print(task.status.value);
+    });
+
+    await dl.whenComplete(url6);
+  });
+
+  test('download in batch', () async {
+    var dl = DownloadManager();
+
+    var urls = <String>[];
+    urls.add(url2);
+    urls.add(url3);
+    urls.add(url);
+
+    dl.addDownload(url2, "./test2.ipa");
+    dl.addDownload(url3, "./test3.ipa");
+    dl.addDownload(url, "./test.ipa");
+
+    var downloadProgress = dl.getDownloadProgress(urls);
+
+    downloadProgress.addListener(() {
+      print(downloadProgress.value);
+    });
+
+    await dl.whenCompletes(urls);
+  });
+
+  test('cancel a batched download', () async {
+    var dl = DownloadManager();
+
+    var urls = <String>[];
+    urls.add(url6);
+    urls.add(url5);
+    urls.add(url);
+    dl.addDownloads(urls, ".");
+
+    var downloads = dl.getDownloads(urls);
+
+    downloads.forEach((task) {
+      task?.status.addListener(() {
+        print(task.request.url + ", " + task.status.value.toString());
+      });
+    });
+
+    dl.cancelDownloads(urls);
+
+    await dl.whenCompletes(urls);
+  });
+
+  test('cancel a single item in a batched download', () async {
+    var dl = DownloadManager();
+
+    var urls = <String>[];
+    urls.add(url4);
+    urls.add(url3);
+    urls.add(url);
+    dl.addDownloads(urls, "");
+
+    var downloads = dl.getDownloads(urls);
+
+    downloads.forEach((task) {
+      task?.status.addListener(() {
+        print(task.status.value);
+      });
+    });
+
+    dl.cancelDownload(url3);
+
+    await dl.whenCompletes(urls);
   });
 }
